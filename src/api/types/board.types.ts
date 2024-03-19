5563f7fffd7db8d09b12f6fe01a47f2d1cfbb440
Charles <charles.bourque96@gmail.com>
2024-03-19 17:05:34 +0100

    Removed fake data from type files
    Signed-off-by: Charles <charles.bourque96@gmail.com>

--- src/api/types/board.types.ts
+++ src/api/types/board.types.ts
@@ -15,17 +15,3 @@ export type Board = {
     description: string;
   };
 };
-
-/*export const DUMMY_BOARD: Board = {
-  localtime: 1709221055,
-  uptime: 17321,
-  load: [5760, 6080, 5312],
-  memory: {
-    total: 513511424,
-    free: 417607680,
-    shared: 409600,
-    buffered: 4096,
-    available: 413192192,
-    cached: 33972224,
-  },
-};*/
--- src/api/types/systemInfo.types.ts
+++ src/api/types/systemInfo.types.ts
@@ -13,19 +13,3 @@ export type SystemInfo = {
     cached: number;
   };
 };
-
-/*export const DUMMY_SYSTEM_INFO: SystemInfo = {
-  kernel: '6.1.77',
-  hostname: 'c4411ef52d53',
-  system: 'ARMv8 Processor rev 4',
-  model: 'Linksys E8450 (UBI)',
-  board_name: 'linksys,e8450-ubi',
-  rootfs_type: 'squashfs',
-  release: {
-    distribution: 'OpenWrt',
-    version: 'SNAPSHOT',
-    revision: 'r25206+8-d5e2177a6b',
-    target: 'mediatek/mt7622',
-    description: 'OpenWrt SNAPSHOT r25206+8-d5e2177a6b',
-  },
-};*/
