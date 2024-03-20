922b3ec0c43151529dd31ac918d936421c602655
Charles <charles.bourque96@gmail.com>
2024-03-20 10:45:50 +0100

    Dependencies bump, added default 404 handling
    Signed-off-by: Charles <charles.bourque96@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { SmileyXEyes } from '@phosphor-icons/react';
 import { useQueryClient } from '@tanstack/react-query';
 import { ErrorComponent, RouterProvider, createRouter } from '@tanstack/react-router';
+import { Heading } from 'components/Heading';
 import { Spinner } from 'components/Spinner';
 import { routeTree } from 'routeTree.gen.ts';
 
@@ -8,6 +10,12 @@ const router = createRouter({
   defaultPendingComponent: () => (
     <div className="flex h-full w-full content-center items-center justify-center text-center">
       <Spinner size="lg" />
+    </div>
+  ),
+  defaultNotFoundComponent: () => (
+    <div className="flex h-1/2 w-full content-center items-center justify-center text-center">
+      <SmileyXEyes className="h-14 w-14 text-primary-500 dark:text-primary-300" />
+      <Heading size="lg">404 - Not Found</Heading>
     </div>
   ),
   defaultErrorComponent: ({ error }) => <ErrorComponent error={error} />,
