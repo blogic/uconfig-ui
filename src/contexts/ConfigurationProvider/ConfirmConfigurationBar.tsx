3597618c0e832a9914f8c749f5160da538fd4903
Charles <charles.bourque96@gmail.com>
2024-03-27 11:27:06 +0100

    Created ConfigurationProvider, ConfirmConfigurationChangesBar component and added it to protected route and MainPageContainer
    Signed-off-by: Charles <charles.bourque96@gmail.com>

--- src/contexts/ConfigurationProvider/ConfirmConfigurationBar.tsx
+++ src/contexts/ConfigurationProvider/ConfirmConfigurationBar.tsx
@@ -7,7 +7,7 @@ export const ConfirmConfigurationChangeBar = () => {
   if (!isDirty) return null;
 
   return (
-    <div className="bg-red-500 p-4 text-center text-white">
+    <div className="bg-red-500 p-4 text-center text-black">
       <p>You have unsaved changes</p>
     </div>
   );
--- src/layout/MainPageContainer.tsx
+++ src/layout/MainPageContainer.tsx
@@ -7,8 +7,10 @@ export type MainPageContainerProps = {
 export const MainPageContainer = ({ children }: MainPageContainerProps) => (
   <main className="z-0 flex w-full flex-1 items-center justify-center overflow-y-auto focus:outline-none">
     <div className="relative w-[600px] px-6 py-6">
-      <ConfirmConfigurationChangeBar />
-      <div className="mt-12 lg:mt-0">{children}</div>
+      <div className=" mt-8 lg:mt-0">
+        <ConfirmConfigurationChangeBar />
+        {children}
+      </div>
     </div>
   </main>
 );
